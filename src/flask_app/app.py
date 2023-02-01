edd5352e16a3bb1755d517e7d9e1832e3571b661
Isabella Vecchi Ferreira <vecchisabella@gmail.com>
2023-01-31 21:57:14 -0300

    nosso app

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -1,1 +1,51 @@
-def comprarPassagem()
+#voos
+# json_voo = {id:"1",partida:"uberlandia", destino:"sao paulo", diaPartida:"22/07/2023", horarioPartida:"19:00",diaChegada:"25/11/2023", horarioChegada:"19:00", valor:"250,00" ,passagens:"25"}
+# retornar somente as que passagens != 0 
+
+#passagens
+# {id:"1",nomePassageiro:"Joao", cpf:"1111111111", viagem:{id:"1",partida:"uberlandia", destino:"sao paulo", diaPartida:"22/07/2023", horarioPartida:"19:00",diaChegada:"25/11/2023", horarioChegada:"19:00", valor:"250,00"}}
+
+#aeroportos
+# {id:"1",nomeAeroporto:"congonhas",cidade:"uberlandia", estado:"MG"}
+
+#ajeitar doc .env com as variaveis de banco
+
+def deletePassagem():
+    # aumentar uma vaga no voo
+    pass
+
+def updateVoo():
+    pass
+
+def deleteVoo():
+    # delete passagens
+    pass
+
+def deleteAeroporto():
+    #delete voos e passagens
+    pass
+
+def deleteUsuario():
+    pass
+
+def cadastrar():
+    pass
+
+def login():
+    pass
+
+def criarAeroporto():
+    pass
+
+def listarAeroportosComPassagensDisponiveis():
+    pass
+
+def criarNovoVoo(json_voo):
+    pass
+
+novaPassagem_json = '{id_voo:"1", nome:"nome", cpf:"11111111111"}'
+def venderPassagem(novaPassagem_json):
+    pass
+
+def listarPassagens():
+    pass
